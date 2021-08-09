316a87e07469e0379b88d370547d9961622832a7
manish.jasaiwal <manish12jes@gmail.com>
2021-08-09 13:08:42 +0530

    1st commit

--- controllers/authController.go
+++ controllers/authController.go
@@ -1,13 +1,13 @@
 package controllers
 
 import(
-	"errors"
+	// "errors"
 	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
-func authAdmin(e echo.Context) error {
+func authAdmin(c echo.Context) error {
 	fmt.Println("Hi, Inside authAdmin controller")
 	c.String(http.StatusOK, "Hi, Inside authAdmin controller")
 	return nil
